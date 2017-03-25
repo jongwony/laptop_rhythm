eaf728b6361b56a459043d18e81c353f92fd3b44
Jongwon <lastone9182@gmail.com>
2017-03-26 01:07:57 +0900

    powershell run script

---
+++ laptop_rhythm.ps1
@@ -0,0 +1,8 @@
+# Windows Powershell Script
+# > ...laptop_rhythm.ps1
+
+# virtualenv activate
+. $PSScriptRoot\_rhythm\Scripts\activate.ps1
+
+# module running
+python $PSScriptRoot\laptop_rhythm.py
